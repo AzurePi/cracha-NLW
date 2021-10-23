8f87470e616953d39eec1cc261eba4664af578a9
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2021-10-23 18:21:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function getGitHubProfileInfo() {
+  const url = 'https://api.github.com/users/AzurePi'
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfo()
